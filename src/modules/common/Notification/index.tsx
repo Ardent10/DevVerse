e12a7ea5766f84300330d480d96317f2cd4cdb6c
Zaky <zak786khan@gmail.com>
2023-06-14 01:43:57 +0530

    Feat: Signup, Login, posts

--- src/modules/common/Notification/index.tsx
+++ src/modules/common/Notification/index.tsx
@@ -1,12 +1,12 @@
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
-interface NotificationProps {
-  message: string;
-  type: "info" | "success" | "warning" | "error";
-}
-export function Notification({ message }: NotificationProps) {
-  toast(message);
+// interface NotificationProps {
+//   message: string;
+//   type: "info" | "success" | "warning" | "error";
+// }
+// export function Notification({ message }: NotificationProps) {
+//   toast(message);
 
-  return <ToastContainer position="top-right" autoClose={5000}/>;
-}
+//   return <ToastContainer position="top-right" autoClose={5000}/>;
+// }
